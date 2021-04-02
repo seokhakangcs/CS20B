b7cd5bee79bb1466679133d1bfd40fc5a86bf971
SeokhaKangCS <SeokhaKangCS@gmail.com>
2021-04-02 14:53:42 +0900

    a

--- asmgt_4/Assignment_4_2.py
+++ asmgt_4/Assignment_4_2.py
@@ -5,10 +5,20 @@ def create_x(t, w, n, d):
     return [t*x + w*n for x in range(d)]
 
 
-def getMaxFromList(datas):
-    max = datas[0]
-    for data in datas:
-        max = max if max > data else data
+def getMaxIndexFromList(datas):
+    max_index = 0
+    for i in range(len(datas)):
+        if datas[i] > datas[max_index]:
+            max_index = i
+    return max_index
+
+
+def getMinIndexFromList(datas):
+    min_index = 0
+    for i in range(len(datas)):
+        if datas[i] < datas[min_index]:
+            min_index = i
+    return min_index
 
 
 readSet1 = open("/Users/kang/Documents/CS20B/asmgt_4/rainfallISet1.txt", "r")
@@ -45,10 +55,17 @@ max_data_1 = data_value_2[0]
 value_a_x = create_x(3, 0.8, 1, 25)
 value_b_x = create_x(3, 0.8, 2, 25)
 
-colors = ['y'] * 25
+print(getMaxIndexFromList(data_value_2))
+
+colors_1 = ['y'] * 25
+colors_2 = ['y'] * 25
+colors_1[getMaxIndexFromList(data_value_1)] = 'b'
+colors_1[getMinIndexFromList(data_value_1)] = 'r'
+colors_2[getMaxIndexFromList(data_value_2)] = 'b'
+colors_2[getMinIndexFromList(data_value_2)] = 'r'
 bx = plt.subplot()
-bx.bar(value_c, data_value_1, color=colors)
-bx.bar(value_d, data_value_2, color=colors)
+bx.bar(value_c, data_value_1, color=colors_1)
+bx.bar(value_d, data_value_2, color=colors_2)
 bx.set_xticks(value_c + value_d)
 bx.set_xticklabels(data_name_1 + data_name_2, rotation=90)
 
